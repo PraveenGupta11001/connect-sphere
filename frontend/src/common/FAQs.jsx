eca71cc1f975d6596bbc7f73414606ac9feeb7ed
PraveenGupta11001 <praweengupta11001@gmail.com>
2025-05-15 23:02:23 +0530

    faq:rest responsiveness

--- frontend/src/common/FAQs.jsx
+++ frontend/src/common/FAQs.jsx
@@ -6,16 +6,16 @@ export default function FAQs() {
     let [expandAns, setExpandAns] = useState('');
 
     return(
-        <div className='m-2'>
-            <h1 className='ml-4 pt-4 mb-2 text-xl text-indigo-500 font-bold '>FAQs(Frequently Asked Questions)</h1>
+        <div className='w-[100%] lg:w-[70%]  m-2 md:text-xl flex flex-col'>
+            <h1 className='ml-4 pt-4 mb-2 text-2xl text-indigo-500 font-bold '>FAQs(Frequently Asked Questions)</h1>
             {faq_question.faqs.map( (item, i) => (
                 <div className='mx-8' key={i}>
-                    <div className='pt-2 flex justify-between text-indigo-500 font-semibold'>
-                        <h3 className='w-[90%]'>Q{i+1} {item.question}</h3>
+                    <div className='pt-2 flex justify-between lg:justify-start text-indigo-500 font-semibold'>
+                        <h3 className=''>Q{i+1} {item.question}</h3>
                         {expandAns!==item.answer?<ChevronDown onClick={()=>setExpandAns(item.answer)}/>:<ChevronUp onClick={()=>setExpandAns('')}/>}
                     </div>
-                    <div className='pb-2 flex justify-between'>
-                        {expandAns==item.answer?<p className='max-w-2xl m-2'>{item.answer}</p>:''}
+                    <div className='pb-2 md:text-md  flex justify-between'>
+                        {expandAns==item.answer?<p className=' m-2'>{item.answer}</p>:''}
                     </div>
                 </div>
                 
